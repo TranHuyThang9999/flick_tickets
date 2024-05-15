0a29944e5063f584b0d59c59046fc040bd1640b8
huythang <tranhuythang9999@gmail.com>
2024-05-15 08:47:42 +0700

    ok spin

--- src/common/customers/LoginWithEmail.jsx
+++ src/common/customers/LoginWithEmail.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Spin } from 'antd';
 import React, { useState } from 'react';
 import { GooglePlusSquareFilled } from '@ant-design/icons';
 import './index.css';
@@ -8,6 +8,7 @@ import { showError } from '../log/log';
 
 export default function LoginWithEmail() {
     const [isNextFormOtp, setIsNextFormOtp] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [form] = Form.useForm();
 
     const layout = {
@@ -16,6 +17,7 @@ export default function LoginWithEmail() {
     };
 
     const handleFormSubmit = async (values) => {
+        setLoading(true);
         try {
             const response = await axios.post(`http://localhost:8080/manager/customer/send/${values.email}`);
             if (response.data.result.code === 0) {
@@ -27,6 +29,8 @@ export default function LoginWithEmail() {
         } catch (error) {
             console.error(error);
             showError('Failed to send request');
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -36,21 +40,23 @@ export default function LoginWithEmail() {
 
     return (
         <div>
-            <Form {...layout} form={form} className="form-container-register-with-email" onFinish={handleFormSubmit}>
-                <GooglePlusSquareFilled style={{ fontSize: '25px' }} />
-                <Form.Item
-                    label='Email'
-                    name="email"
-                    rules={[{ required: true, type: 'email', message: 'Vui lòng nhập địa chỉ email!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                        Tiếp tục
-                    </Button>
-                </Form.Item>
-            </Form>
+            <Spin spinning={loading} tip="Vui lòng chờ...">
+                <Form {...layout} form={form} className="form-container-register-with-email" onFinish={handleFormSubmit}>
+                    <GooglePlusSquareFilled style={{ fontSize: '25px' }} />
+                    <Form.Item
+                        label='Email'
+                        name="email"
+                        rules={[{ required: true, type: 'email', message: 'Vui lòng nhập địa chỉ email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            Tiếp tục
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Spin>
         </div>
     );
 }
